b4cd5b6709accb88fed234616827336af65d535e
gynflo <ginet.florian.dev@gmail.com>
2024-07-01 21:41:11 +0200

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      script: "npm start",
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "192.168.1.1.165",
+      ref: "origin/main",
+      repo: "https://github.com/Devgynflo/nextjs_pm2",
+      path: "/home/ubuntu/",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+      "ssh-options": "ForwardAgent=yes",
+    },
+  },
+};
